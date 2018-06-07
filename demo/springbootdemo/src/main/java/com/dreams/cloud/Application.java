70f8ad62d076b732baecf6754f53a94b4d002e79
gitzhan <260597116@qq.com>
2018-06-07 10:28:40 +0800

    demo

---
+++ demo/springbootdemo/src/main/java/com/dreams/cloud/Application.java
@@ -0,0 +1,20 @@
+package com.dreams.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(String msg) {
+		return "hello springboot "+msg;
+	}
+}
